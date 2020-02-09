419758845f51c818b891c6d6cd4afedb901fc019
liamorourkemorgan <liamorourkemorgan@gmail.com>
2020-02-09 15:34:26 +0000

    adding form array, and also validation

--- inventory-manager/src/app/components/item-form/item-form.component.ts
+++ inventory-manager/src/app/components/item-form/item-form.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormBuilder, FormGroup, FormArray, Validators } from '@angular/forms';
+
+import axios from 'axios'
+
+import { app } from '../../../config/app'
+
 @Component({
   selector: 'app-item-form',
   templateUrl: './item-form.component.html',
@@ -7,9 +13,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemFormComponent implements OnInit {
 
-  constructor() { }
+  itemForm: FormGroup;
 
-  ngOnInit() {
+  loading = false;
+  submitted = false;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit () {
+    this.itemForm = this.fb.group({
+      items: this.fb.array([])
+    })
+
+    this.itemForm.valueChanges.subscribe(console.log)
+  }
+
+  get itemForms () {
+    return this.itemForm.get('items') as FormArray
+  }
+
+  addItem () {
+    const item = this.fb.group({
+      itemName: ['', [
+        Validators.required
+      ]],
+      quality: ['', [
+        Validators.required,
+        Validators.max(50)
+      ]],
+      sellIn: ['', [
+        Validators.required
+      ]]
+    })
+
+    this.itemForms.push(item);
+  }
+
+  get items (): FormArray {
+    return this.itemForm.get('items') as FormArray;
+  }
+
+  deleteItem (i) {
+    this.itemForms.removeAt(i)
+  }
+
+  async handleSubmit () {
+    this.loading = true
+
+    const values = this.itemForm.value;
+
+    try {
+      await axios.post(app.ItemServiceURL`/gettomorrow/values`, values)
+    } catch (e) {
+      console.error(e)
+    }
   }
 
 }
